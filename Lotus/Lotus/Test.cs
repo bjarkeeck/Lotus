745624e89f33413bfbecad26f2d112bf140ba1e1
TheLogan <timlogan475@gmail.com>
2013-12-13 13:52:52 +0100

    Test

--- Lotus/Lotus/Test.cs
+++ Lotus/Lotus/Test.cs
@@ -6,23 +6,19 @@ using System.Threading.Tasks;
 using LotusEngine;
 using System.Drawing;
 
-namespace Lotus
-{
-    class Test : Component
-    {
-        public override void Update()
-        {
-            transform.rotation += 20 * Time.DeltaTime;
-        }
+namespace Lotus {
+	class Test : Component {
+		public override void Update () {
+			transform.rotation += 20 * Time.DeltaTime;
+		}
+		//
+		public override void Draw () {
+			Rendering.DrawLine (Vector2.zero, new Vector2 (Settings.Screen.Width / 2, Settings.Screen.Height / 2), 2, Color.Red);
 
-        public override void Draw()
-        {
-            Rendering.DrawLine(Vector2.zero, new Vector2(Settings.Screen.Width / 2, Settings.Screen.Height / 2), 2, Color.Red);
+			var intersects = Collider.LineIntersects (Vector2.zero, new Vector2 (Settings.Screen.Width / 2, Settings.Screen.Height / 2));
 
-            var intersects = Collider.LineIntersects(Vector2.zero, new Vector2(Settings.Screen.Width / 2, Settings.Screen.Height / 2));
-
-            foreach (var intersect in intersects)
-                Rendering.DrawCircle(intersect, 5, 2, Color.Red);
-        }
-    }
+			foreach (var intersect in intersects)
+				Rendering.DrawCircle (intersect, 5, 2, Color.Red);
+		}
+	}
 }
