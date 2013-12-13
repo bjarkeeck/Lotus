0e6914f466607cb8b9dc351d510f29b44bfe2d53
bjarkeck <bjarkeelias@gmail.com>
2013-12-13 12:55:35 +0100

    asdasd

--- Lotus/Lotus/Program.cs
+++ Lotus/Lotus/Program.cs
@@ -8,7 +8,7 @@ namespace Lotus
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application..
         /// </summary>
         [STAThread]
         static void Main()
